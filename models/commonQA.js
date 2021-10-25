21ff434b6c92dadd211ae5ba394ff207db825226
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2021-10-25 15:49:23 +0800

    branch feat: create commonQAs's Schema  and related seed data.

---
+++ models/commonQA.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+// setup schema
+const commonQASchema = new Schema({
+  question: {
+    type: String,
+    required: true,
+    // unique: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+    // unique: true,
+  },
+  imgUrls: [String],
+});
+
+// generate createdAt and updatedAt fields automatically
+commonQASchema.set("timestamps", true);
+
+// export model
+export default mongoose.model("Common_QA", commonQASchema);
