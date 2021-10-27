7882341922ce9b9b37382f4b611c26b8a372da75
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2021-10-27 22:26:49 +0800

    feat: create post schema, seeder and routes.

---
+++ models/post.js
@@ -0,0 +1,98 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  itemName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: true,
+  },
+  itemStatus: {
+    type: String,
+    enum: ["全新", "二手"],
+    required: true,
+  },
+  description: String,
+  tradingOptions: {
+    convenientStore: {
+      storeCode: { type: Number, match: /^\d{5, 6}$/ }, //6碼=7-11、全家, 5碼=全家、萊爾富、OK
+      storeName: { type: String },
+      fee: {
+        type: Number,
+        default: 60,
+      },
+    },
+    faceToFace: {
+      region: {
+        type: String,
+        enum: [
+          "基隆市",
+          "臺北市",
+          "新北市",
+          "桃園市",
+          "宜蘭縣",
+          "新竹市",
+          "新竹縣",
+          "苗栗縣",
+          "台中市",
+          "彰化縣",
+          "南投縣",
+          "花蓮縣",
+          "雲林縣",
+          "嘉義縣",
+          "臺南市",
+          "高雄市",
+          "台東縣",
+          "屏東縣",
+          "澎湖縣",
+          "金門縣",
+          "連江縣",
+        ],
+      },
+      district: {
+        type: String,
+      },
+      fee: {
+        type: Number,
+        default: 0,
+      },
+    },
+    // logisticsExpress: {company:{type: String, enum:["黑貓宅急便", "新竹物流", "台灣便利通"] }, fee: {type: Number, default: 100}, }
+  },
+
+  payer: {
+    type: String,
+    //enum: ["刊登者", "索取者", "贈與者"]
+    default: "索取者",
+    //required: true,
+  },
+  isGoal: {
+    type: Boolean,
+    default: false,
+  },
+  isPublic: {
+    type: Boolean,
+    default: true,
+  },
+  //imgUrls: String,
+  // ownerId: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  //   index: true,
+  //   required: true,
+  // },
+  // categoryId: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "Category",
+  //   index: true,
+  //   required: true,
+  // },
+});
+
+postSchema.set("timestamps", true);
+
+export default mongoose.model("Post", postSchema);
---
+++ routes/modules/posts.js
@@ -0,0 +1,132 @@
+import express from "express";
+import Post from "../../models/post.js";
+
+const router = express.Router();
+
+//READ all posts
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await Post.find();
+    if (allPosts) {
+      res.status(200).json(allPosts);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//READ a post
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      return res
+        .status(404)
+        .json({ error: "The post you are looking for does not exist." });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//CREATE a post
+router.post("/", async (req, res) => {
+  //TODO: user authentication
+  //const userId = user._id
+  const {
+    itemName,
+    quantity,
+    itemStatus,
+    description,
+    storeCode,
+    storeName,
+    region,
+    district,
+  } = req.body;
+  const dataStructure = {
+    itemName,
+    quantity,
+    itemStatus,
+    description,
+  };
+  let tradingOptions = {};
+  if ((storeCode && storeName) || (region && district)) {
+    if (storeCode && storeName) {
+      tradingOptions.convenientStore = { storeCode, storeName };
+    }
+    if (region && district) {
+      tradingOptions.faceToFace = { region, district };
+    }
+  }
+  dataStructure.tradingOptions = tradingOptions;
+  try {
+    const addPost = await Post.create(dataStructure);
+    if (addPost) {
+      res.status(200).json(addPost);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//UPDATE a post
+router.put("/:id", async (req, res) => {
+  // TODO: user authentication
+  const { id } = req.params;
+  //const userId = user._id; const _id = req.params.id
+  const {
+    itemName,
+    quantity,
+    itemStatus,
+    description,
+    storeCode,
+    storeName,
+    region,
+    district,
+  } = req.body;
+  const dataStructure = {
+    itemName,
+    quantity,
+    itemStatus,
+    description,
+  };
+  let tradingOptions = {};
+  if ((storeCode && storeName) || (region && district)) {
+    if (storeCode && storeName) {
+      tradingOptions.convenientStore = { storeCode, storeName };
+    }
+    if (region && district) {
+      tradingOptions.faceToFace = { region, district };
+    }
+  }
+  dataStructure.tradingOptions = tradingOptions;
+  try {
+    //const updatePost = await Post.findOneAndUpdate({_id: ObjectId(id), userId: userId});
+    const updatePost = await Post.findByIdAndUpdate(id, dataStructure, {
+      runValidators: true,
+      new: true,
+    });
+    if (updatePost) {
+      res.status(200).json(updatePost);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//DELETE a post
+router.delete("/:id", async (req, res) => {
+  // TODO: user authentication
+  const { id } = req.params;
+  try {
+    await Post.findByIdAndDelete(id);
+    res.status(200).json({ message: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
